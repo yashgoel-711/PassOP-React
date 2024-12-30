64166aed55363e93e90c39ececa105e940da330f
yashgoel711@gmail.com <yashgoel711@gmail.com>
2024-12-30 20:04:52 +0530

    final UI Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,19 @@ function App() {
 
   return (
     <>
-    <div className='h-[100vh] w-[100vw] '>
-    <div className="relative h-[100%] w-[100%]  bg-slate-950 text-green-700">
+    <div className="relative h-[100%] max-w-[100%] overflow-x-hidden  bg-slate-950 text-green-700">
         <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
         <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+    
+    <div className='min-h-[100vh] '>
     <header>
       <Navbar />      
     </header>
-    <main>
+    <main className='mb-10'>
       <Body />
     </main>
     </div>
+
     </div>
     </>
   )
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import lottie from "lottie-web";
 import { defineElement } from "@lordicon/element";
 import { useRef, useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
+
 
 // define "lord-icon" custom element with default properties
 defineElement(lottie.loadAnimation);
 
 const Body = () => {
-  const ref = useRef();  
+  const ref = useRef();
   const ref2 = useRef();
   const [Profiles, setProfiles] = useState([])
-  
+
   const [Profile, setProfile] = useState({
     url: '',
     username: '',
@@ -23,26 +26,38 @@ const Body = () => {
       setProfiles(JSON.parse(data))
     }
   }, [])
-  
+
   const handleChange = (e) => {
     setProfile({
       ...Profile, [e.target.name]: e.target.value
     })
   }
-  
-  const handleAdd = () => {    
+
+  const handleAdd = () => {
     const updatedProfiles = [...Profiles, Profile];
     setProfiles(updatedProfiles);
     setProfile({
       url: '',
       username: '',
       password: ''
-    }) 
+    })
     localStorage.setItem("Profiles", JSON.stringify(updatedProfiles));
   }
-  const handleCopy = () => {
-    navigator.clipboard.writeText(ref2.current.parentElement.innerText);
-    
+  const handleCopy = (text) => {
+    toast.success(' Copied To Clipboard!', {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Slide,
+      
+    });
+    navigator.clipboard.writeText(text);
+
   }
 
   const showPassword = () => {
@@ -60,7 +75,21 @@ const Body = () => {
     }
   }
   return (
-    <div className='md:w-[60vw] mx-auto min-h-[60vh] bg-[#efcdff17]  rounded-lg mt-10 p-10'>
+    <div className='md:w-[60vw] mx-auto min-h-[60vh] bg-[#efcdff17] rounded-lg mt-10 p-10'>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Slide}
+        
+      />
       <div className='flex flex-col gap-4 mb-6'>
 
         <div className='' >
@@ -72,13 +101,13 @@ const Body = () => {
 
           <input onChange={handleChange} type="Password" value={Profile.password} className='outline-none  border-2 border-blue-500 focus:ring-2 focus:ring-green-500 rounded-full px-3 md:w-1/2' placeholder='Enter Password' name="password" id="inp-pass" required />
           <span className='right-[-8px] top-[-7px] absolute cursor-pointer' onClick={showPassword}>
-            <lord-icon ref={ref} src="/src/assets/icons/eye-blink.json" trigger="loop" delay="1000" state="hover-blink" style={{width : "100px", height : "45px"}}></lord-icon>
+            <lord-icon ref={ref} src="/src/assets/icons/eye-blink.json" trigger="loop" delay="1000" state="hover-blink" style={{ width: "100px", height: "45px" }}></lord-icon>
           </span>
 
         </div>
 
         <div className='flex justify-center '>
-          <button onClick={handleAdd}  className=' flex items-center px-3 py-1 rounded-full gap-2 bg-black hover:text-violet-500'>
+          <button onClick={handleAdd} className=' flex items-center px-3 py-1 rounded-full gap-2 bg-black hover:text-violet-500'>
             <lord-icon trigger="morph" src="/src/assets/icons/folder.json"></lord-icon>
             <h1 className='text-green-500'>Add Password</h1>
           </button>
@@ -90,44 +119,54 @@ const Body = () => {
         <h1 className='bg-gradient-to-r from-indigo-500 w-[30%] h-[1.9px] '></h1>
       </div>
 
-      
-        <div className="profiles w-[90%] mt-8">
-         
 
-          <table className='text-center table-fixed w-full mx-auto bg-gradient-to-r from-slate-800 to-slate-900 text-white '>
-            {Profiles.map((profile) => {
-              return (
-                <>
-              <tr>  
-                <td className='break-words border-2 border-green-500 relative'>
-                  <div className="">
-                  <a target='_blank' href={profile.url}><h1 className='p-4 '>{profile.url}</h1></a>
-                  <lord-icon  ref={ref2} onClick={handleCopy} src="/src/assets/icons/copy.json" trigger="hover"  state="hover-slide" ></lord-icon>
-                  </div>
-                </td>
-                <td className='break-words border-2 border-green-500 relative'>
-                  <div  className="">
-                  <h1  className='p-4'>{profile.username}</h1>
-                  <lord-icon  ref={ref2} onClick={handleCopy} src="/src/assets/icons/copy.json" trigger="hover"  state="hover-slide" ></lord-icon>
-                  </div>
+      <div className="profiles w-[90%] mt-8">
+
+
+        <table className='border-2 border-green-500 text-center table-fixed w-full mx-auto bg-gradient-to-r from-slate-800 to-slate-900 text-white '>
+                <tr>
+                  <th>Site</th>
+                  <th>Username</th>
+                  <th>Password</th>
+                  <th>Actions</th>
+                </tr>
+          {Profiles.map((profile) => {
+            return (
+              <>
+                <tr>
+                  <td className='break-words border-2 border-green-500 '>
+                    <a target='_blank' href={profile.url}><h1 className='p-4 '>{profile.url}</h1></a>
+                    <div className="cursor-pointer">
+                      <lord-icon onClick={() => { handleCopy(profile.url) }} src="/src/assets/icons/copy.json" trigger="hover" state="hover-slide" ></lord-icon>
+                    </div>
                   </td>
-                <td className='break-words border-2 border-green-500 relative'>
-                  <div  className="">
-                  <h1  className='p-4'>{profile.password}</h1>
-                  <lord-icon  ref={ref2} onClick={handleCopy} src="/src/assets/icons/copy.json" trigger="hover"  state="hover-slide" ></lord-icon>
-                  </div>
+                  <td className='break-words border-2 border-green-500 '>
+                    <h1 className='p-4'>{profile.username}</h1>
+                    <div className="cursor-pointer">
+                      <lord-icon onClick={() => { handleCopy(profile.username) }} src="/src/assets/icons/copy.json" trigger="hover" state="hover-slide" ></lord-icon>
+                    </div>
                   </td>
-              </tr>
-            </>
+                  <td className='break-words border-2 border-green-500 '>
+                    <h1 className='p-4'>{profile.password}</h1>
+                    <div className="cursor-pointer">
+                      <lord-icon onClick={() => { handleCopy(profile.password) }} src="/src/assets/icons/copy.json" trigger="hover" state="hover-slide" ></lord-icon>
+                    </div>
+                  </td>
+                  <td className='break-words border-2 border-green-500 '>
+                      <lord-icon onClick={() => { handleCopy(profile.password) }} src="/src/assets/icons/edit.json" trigger="loop" state="hover-circle" ></lord-icon>
+                    
+                  </td>
+                </tr>
+              </>
             )
           })}
-          </table>
-          </div>
+        </table>
+      </div>
 
-        </div>
-     
+    </div>
 
-  
+
+
 
   )
 }
